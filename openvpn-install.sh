1f47f8e7438d000beb01491d8bf809c972c59dcc
ponas <47662158+girteka@users.noreply.github.com>
2019-11-22 18:01:51 +0200

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1220,35 +1220,35 @@ function manageMenu () {
 	echo "Welcomeeeee to OpenVPN-install!"
 	echo "The git repository is available at: https://github.com/angristan/openvpn-install"
 	echo ""
-	echo "It looks like OpenVPN is already installed."
+	echo "Atrodo OpenVPN jau įdiegta."
 	echo ""
-	echo "What do you want to do?"
-	echo "   1) Addooooo a new user"
-	echo "   2) Revoke existing user"
-	echo "   3) Remove OpenVPN"
-	echo "   4) Exit"
+	echo "Ką nori daryti?"
+	echo "   1) Pridėti naują vartotoją"
+	echo "   2) Panaikinti esamą vartotoją"
+	echo "   3) Pašalinti OpenVPN"
+	echo "   4) "
 	until [[ "$MENU_OPTION" =~ ^[1-4]$ ]]; do
 		read -rp "Select an option [1-4]: " MENU_OPTION
 	done
 
 	case $MENU_OPTION in
 		1)
-			newClient
+			naujasVartotojas
 		;;
 		2)
-			revokeClient
+			PanaikintiVartotoja
 		;;
 		3)
-			removeOpenVPN
+			PašalintiOpenVPN
 		;;
 		4)
-			exit 0
+			Išeiti 0
 		;;
 	esac
 }
 
 # Check for root, TUN, OS...
-initialCheck
+Patikrinimas
 
 # Check if OpenVPN is already installed
 if [[ -e /etc/openvpn/server.conf ]]; then
